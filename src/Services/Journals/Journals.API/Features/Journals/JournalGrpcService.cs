f946fac594ab26bc30b6852e007fe2e5564b71d9
Alnuimi <italnuimi@gmail.com>
2026-04-04 17:54:53 +0300

    feat: Implement gRPC service for journal editor assignment and add necessary extensions

---
+++ src/Services/Journals/Journals.API/Features/Journals/JournalGrpcService.cs
@@ -0,0 +1,20 @@
+﻿using Blocks.Redis;
+using Journals.Domain.Journals;
+using Journals.Grpc;
+using ProtoBuf.Grpc;
+
+namespace Journals.API.Features.Journals;
+
+public sealed class JournalGrpcService(Repository<Journal> _journalRepository) : IJournalService
+{
+    public async ValueTask<IsEditorAssignedToJournalResponse> IsEditorAssignedToJournalAsync(
+        IsEditorAssignedToJournalRequest request, CallContext context = default)
+    {
+        var journal = await _journalRepository.GetByIdOrThrowAsync(request.JournalId);
+
+        return new IsEditorAssignedToJournalResponse
+        {
+            IsAssigned = journal.ChiefEditorId == request.UserId;
+        };
+    }
+}
