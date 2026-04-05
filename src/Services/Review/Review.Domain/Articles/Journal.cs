f0a0ab7abf8663a167eddbeb18cb75d8f7028a2d
Alnuimi <italnuimi@gmail.com>
2026-04-05 23:12:36 +0300

    feat(Articles): Add Articles domain entities(ArticleActor, Author, ArticleAuthor, Editor, Journal, Reviewer, ReviewerSpecialization)

---
+++ src/Services/Review/Review.Domain/Articles/Journal.cs
@@ -0,0 +1,9 @@
+﻿using Blocks.Domain.Entities;
+
+namespace Review.Domain.Articles;
+
+public class Journal : Entity
+{
+    public required string Name { get; set; }
+    public required string Abbreviation { get; set; }
+}
