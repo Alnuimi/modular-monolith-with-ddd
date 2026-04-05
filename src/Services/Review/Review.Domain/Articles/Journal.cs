9ddbad364a18354c9545264da522272cc5caa116
Alnuimi <italnuimi@gmail.com>
2026-04-06 00:55:46 +0300

    refactor(Journal): Add Articles collection to Journal entity in Review.Domain

--- src/Services/Review/Review.Domain/Articles/Journal.cs
+++ src/Services/Review/Review.Domain/Articles/Journal.cs
@@ -6,4 +6,7 @@ public class Journal : Entity
 {
     public required string Name { get; set; }
     public required string Abbreviation { get; set; }
+    private readonly List<Article> _articles = new();
+    
+    public IReadOnlyList<Article> Articles => _articles.AsReadOnly();
 }
