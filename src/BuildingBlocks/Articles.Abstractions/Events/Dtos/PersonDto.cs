25f352a04ab8598fc2b91556680366ab140c652f
Alnuimi <italnuimi@gmail.com>
2026-04-05 17:15:16 +0300

    feat(Dtos): Add DTOs for article events

---
+++ src/BuildingBlocks/Articles.Abstractions/Events/Dtos/PersonDto.cs
@@ -0,0 +1,11 @@
+﻿namespace Articles.Abstractions.Events.Dtos;
+
+public sealed record PersonDto(
+    int Id,
+    string FirstName,
+    string LastName,
+    string Email,
+    string? Honorific,
+    string? Affiliation,
+    int? UserId,
+    string TypeDiscriminator);
