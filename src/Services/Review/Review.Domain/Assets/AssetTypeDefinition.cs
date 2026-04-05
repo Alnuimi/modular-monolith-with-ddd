310f5a940f4999a27c6bf7d7c651628ee68a3be7
Alnuimi <italnuimi@gmail.com>
2026-04-06 00:07:07 +0300

    feat(Asset): Add Assets domain entities(Asset, AssetTypeDefinition) and value objects (AssetName, File, FileName, FileExtension, FileExtensions) and enums (AssetState) to manage article assets in the Review service

---
+++ src/Services/Review/Review.Domain/Assets/AssetTypeDefinition.cs
@@ -0,0 +1,13 @@
+﻿using Articles.Abstractions.Enums;
+using Blocks.Domain.Entities;
+using Review.Domain.Assets.ValueObjects;
+
+namespace Review.Domain.Assets;
+
+public class AssetTypeDefinition : EnumEntity<AssetType>
+{
+    public required FileExtensions AllowedFileExtensions { get; init; }
+    public required string DefaultFileExtension { get; init; } = default!;
+    public required byte MaxFileSizeInMegabytes { get; init; }
+    public required byte MaxAssetCount {get; init;}
+}
---
+++ src/Services/Review/Review.Domain/Assets/ValueObjects/File.cs
@@ -0,0 +1,13 @@
+﻿using Blocks.Domain.ValueObjects;
+using Review.Domain.Assets.ValueObjects;
+
+namespace Review.Domain.Assets;
+
+public partial class File : IValueObject
+{
+    public required string OriginalName { get; init; } = default!;
+    public required string FileServerId { get; init; } = default!;
+    public required long Size { get; init; }
+    public required FileName Name { get; init; }
+    public required FileExtension Extension { get; init; } = default!;   
+}
