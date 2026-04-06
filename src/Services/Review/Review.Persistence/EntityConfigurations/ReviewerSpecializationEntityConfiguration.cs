eebca0884203b0d637f3730dec1338f5a017a140
Alnuimi <italnuimi@gmail.com>
2026-04-07 01:06:40 +0300

    feat: Add ReviewerSpecializationEntityConfiguration to configure the many-to-many relationship between Reviewer and Journal entities in the Review.Persistence layer.

---
+++ src/Services/Review/Review.Persistence/EntityConfigurations/ReviewerSpecializationEntityConfiguration.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Review.Domain.Articles;
+
+namespace Review.Persistence.EntityConfigurations;
+
+internal sealed class ReviewerSpecializationEntityConfiguration : IEntityTypeConfiguration<ReviewerSpecialization>
+{
+    public void Configure(EntityTypeBuilder<ReviewerSpecialization> builder)
+    {
+        builder.HasKey(je => new { je.Journal, je.ReviewerId });
+
+        builder.HasOne(r => r.Journal)
+            .WithMany(j => j.Reviewers)
+            .HasForeignKey(je => je.JournalId);
+
+        builder.HasOne(r => r.Reviewer)
+            .WithMany(j => j.Specializations)
+            .HasForeignKey(je => je.ReviewerId);
+    }
+}
