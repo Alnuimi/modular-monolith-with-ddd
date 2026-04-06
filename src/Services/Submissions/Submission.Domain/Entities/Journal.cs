a4f4a4a29212354cc86903303327a2a66541d635
Alnuimi <italnuimi@gmail.com>
2026-04-07 00:48:23 +0300

    refactor(Journal-Submission): Rename 'Abreviation' to 'Abbreviation' for clarity and correctness on Submission.Domain.Entities.Journal.

--- src/Services/Submissions/Submission.Domain/Entities/Journal.cs
+++ src/Services/Submissions/Submission.Domain/Entities/Journal.cs
@@ -6,7 +6,7 @@ namespace Submission.Domain.Entities;
     public partial class Journal : Entity
     {
         public required string Name { get; set; }
-        public required string Abreviation { get; set; }
+        public required string Abbreviation { get; set; }
     
         private readonly List<Article> _articles = new();
     
