3d260d8bb5adf17be22f83e8b4d6748b6a6bf60d
Alnuimi <italnuimi@gmail.com>
2026-04-07 00:57:37 +0300

    feat(JournalEntityConfiguration-Review): Add Journal entity configuration

---
+++ src/Services/Review/Review.Persistence/EntityConfigurations/JournalEntityConfiguration.cs
@@ -0,0 +1,22 @@
+﻿using Blocks.Core.Constraints;
+using Blocks.EntityFramework.EntityConfigurations;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Review.Domain.Articles;
+
+namespace Review.Persistence.EntityConfigurations;
+
+internal sealed  class JournalEntityConfiguration :  EntityConfiguration<Journal>
+{
+    protected override bool HasGeneratedId => false;
+
+    public override void Configure(EntityTypeBuilder<Journal> builder)
+    {
+        base.Configure(builder);
+        
+        builder.Property(j => j.Name)
+            .HasMaxLength(MaxLength.C64).IsRequired();
+        
+        builder.Property(j => j.Abbreviation)
+            .HasMaxLength(MaxLength.C16).IsRequired();
+    }
+}
