f65d6e5c322a2950cf551d26eea0c026302cd256
Alnuimi <italnuimi@gmail.com>
2026-04-06 23:40:40 +0300

    feat(EditorEntityConfiguration): Add entity configuration for Editor entity, specifying that it inherits from Reviewer.

---
+++ src/Services/Review/Review.Persistence/EntityConfigurations/EditorEntityConfiguration.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using Review.Domain.Articles;
+
+namespace Review.Persistence.EntityConfigurations;
+
+internal sealed class EditorEntityConfiguration : IEntityTypeConfiguration<Editor>
+{
+    public void Configure(EntityTypeBuilder<Editor> builder)
+    {
+        builder.HasBaseType<Reviewer>();
+    }
+}
