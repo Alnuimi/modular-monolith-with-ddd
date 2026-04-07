fad4ff4ff6b032bdfce75cf9f191e31a7d3a0322
Alnuimi <italnuimi@gmail.com>
2026-04-07 20:17:28 +0300

    refactor(ArticleAuthor-Review): Add constructor to set contribution areas in ArticleAuthor class. This allows for easier initialization of ArticleAuthor instances with specific contribution areas, improving code readability and maintainability.

--- src/Services/Review/Review.Domain/Articles/ArticleAuthor.cs
+++ src/Services/Review/Review.Domain/Articles/ArticleAuthor.cs
@@ -4,6 +4,15 @@ namespace Review.Domain.Articles;
 
 public class ArticleAuthor : ArticleActor
 {
+    public ArticleAuthor()
+    {
+        /* empty */
+    }
+
+    public ArticleAuthor(HashSet<ContributionArea> contributionAreas)
+    {
+        _contributionAreas = contributionAreas;
+    }
     internal HashSet<ContributionArea> _contributionAreas = new();
     public IReadOnlyCollection<ContributionArea> ContributionAreas => _contributionAreas;
 }
