b28d245637735b05fe3e41530d857392f4f50263
Alnuimi <italnuimi@gmail.com>
2026-04-07 17:23:46 +0300

    feat: Add global usings for Review.Application project to simplify code and improve readability.

---
+++ src/Services/Review/Review.Application/GlobalUsings.cs
@@ -0,0 +1,15 @@
+﻿// Third-party libraries
+global using Microsoft.EntityFrameworkCore;
+global using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+//Internal libraries
+global using Blocks.Core;
+global using Blocks.EntityFramework;
+global using Articles.Abstractions.Enums;
+
+// Domain
+global using Review.Domain.Articles;
+
+// Persistence
+global using Review.Persistence;
+global using Review.Persistence.Repositories;
