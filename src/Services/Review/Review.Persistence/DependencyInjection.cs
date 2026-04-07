6d09539b4c33d853b7104726a2d0ef1faa09cb87
Alnuimi <italnuimi@gmail.com>
2026-04-07 20:19:09 +0300

    feat: Add DependencyInjection class for Review.Persistence to register services and DbContext to inject dependencies.

---
+++ src/Services/Review/Review.Persistence/DependencyInjection.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using Review.Persistence.Repositories;
+
+namespace Review.Persistence;
+
+public static  class DependencyInjection
+{
+    public static IServiceCollection AddPersistenceServices(this IServiceCollection services,
+        IConfiguration configuration)
+    {
+        var connectionString  = configuration.GetConnectionString("Database");
+        services.AddDbContext<ReviewDbContext>(options =>
+        {
+            options.UseSqlServer(connectionString);
+        });
+        
+        services.AddScoped(typeof(Repository<>));
+        services.AddScoped(typeof(ArticleRepository));
+        services.AddScoped(typeof(AssetTypeDefinitionRepository));
+        
+        return services;
+    }
+}
