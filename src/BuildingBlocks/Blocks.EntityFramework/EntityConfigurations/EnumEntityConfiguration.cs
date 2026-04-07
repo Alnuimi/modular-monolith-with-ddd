1f076009e711190ae3a74d0770af313dd2f29be8
Alnuimi <italnuimi@gmail.com>
2026-04-07 05:02:17 +0300

    feat(EnumEntityConfiguration): Add configuration for enum entities in Entity Framework.

---
+++ src/BuildingBlocks/Blocks.EntityFramework/EntityConfigurations/EnumEntityConfiguration.cs
@@ -0,0 +1,27 @@
+﻿using Blocks.Core.Constraints;
+using Blocks.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Blocks.EntityFramework.EntityConfigurations;
+
+public abstract class EnumEntityConfiguration<T, TEnum> : EntityConfiguration<T, TEnum>
+where T : EnumEntity<TEnum>
+where TEnum : struct, Enum
+{
+    public override void Configure(EntityTypeBuilder<T> builder)
+    {
+        base.Configure(builder);
+        
+        builder.HasIndex(b => b.Name).IsUnique();
+        builder.Property(b => b.Name)
+            .IsRequired()
+            .HasEnumConvesion()
+            .HasMaxLength(MaxLength.C64)
+            .HasColumnOrder(1);
+        builder.Property(b => b.Description)
+                    .IsRequired()
+                    .HasMaxLength(MaxLength.C512)
+                    .HasColumnOrder(2);
+    }
+}
