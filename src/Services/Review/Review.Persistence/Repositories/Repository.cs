9a98cb9b8692b574eddce6f1192b275a7d216ff6
Alnuimi <italnuimi@gmail.com>
2026-04-07 17:05:58 +0300

    feat(Repository-Review): Add Repository class for Review service persistence layer inheriting from generic Repository in Blocks.EntityFramework.

---
+++ src/Services/Review/Review.Persistence/Repositories/Repository.cs
@@ -0,0 +1,10 @@
+﻿using Blocks.Domain.Entities;
+using Blocks.EntityFramework;
+namespace Review.Persistence.Repositories;
+
+public class Repository<TEntity>(ReviewDbContext dbContext) 
+    : Repository<ReviewDbContext, TEntity>(dbContext)
+    where TEntity : class , IEntity
+{
+    // todo we rename Repository to RepositoryBase in Blocks.EntityFramework
+}
