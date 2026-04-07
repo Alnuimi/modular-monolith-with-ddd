5844754159665ac2db881863f04b85eacefb211f
Alnuimi <italnuimi@gmail.com>
2026-04-07 05:13:33 +0300

    fix: Update the order of base.OnModelCreating call in AuthDbContext to ensure configurations are applied correctly in the Auth.Persistence layer.

--- src/Services/Auth/Auth.Persistence/AuthDbContext.cs
+++ src/Services/Auth/Auth.Persistence/AuthDbContext.cs
@@ -13,7 +13,7 @@ public class AuthDbContext(DbContextOptions<AuthDbContext> options)
     public virtual DbSet<Person> Persons { get; set; }
     protected override void OnModelCreating(ModelBuilder builder)
     {
-        base.OnModelCreating(builder);
         builder.ApplyConfigurationsFromAssembly(typeof(AuthDbContext).Assembly);
+        base.OnModelCreating(builder);
     }
 }
