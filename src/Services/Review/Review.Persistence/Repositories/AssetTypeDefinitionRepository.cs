65b696b7f7158bd69719bbef16ec8affbafb5623
Alnuimi <italnuimi@gmail.com>
2026-04-07 20:18:33 +0300

    feat: Add AssetTypeDefinitionRepository to manage asset type definitions in the review service.

---
+++ src/Services/Review/Review.Persistence/Repositories/AssetTypeDefinitionRepository.cs
@@ -0,0 +1,12 @@
+﻿using Articles.Abstractions.Enums;
+using Blocks.EntityFramework;
+using Microsoft.Extensions.Caching.Memory;
+using Review.Domain.Assets;
+
+namespace Review.Persistence.Repositories;
+
+public sealed class AssetTypeDefinitionRepository(ReviewDbContext dbContext, IMemoryCache cache)
+    : CachedRepository<ReviewDbContext, AssetTypeDefinition, AssetType>(dbContext, cache)
+{
+    
+}
