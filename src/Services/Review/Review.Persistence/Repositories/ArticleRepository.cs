217ad07876a514ef066200668b8ccf0e8e21d463
Alnuimi <italnuimi@gmail.com>
2026-04-07 17:12:59 +0300

    feat(ArticleRepository-ReviewPersistence): Add ArticleRepository implementation to Review.Persistence.Repositories. This new repository class extends the base Repository class and provides data access functionality for Article entities within the Review domain.

---
+++ src/Services/Review/Review.Persistence/Repositories/ArticleRepository.cs
@@ -0,0 +1,9 @@
+﻿using Review.Domain.Articles;
+
+namespace Review.Persistence.Repositories;
+
+public sealed class ArticleRepository(ReviewDbContext dbContext)
+    : Repository<Article>(dbContext)
+{
+    
+}
