7ef61128b3f52c9fa88ffa985c4d0694a0633119
Alnuimi <italnuimi@gmail.com>
2026-04-08 18:11:14 +0300

    feat: Add behavior to create File from FileDto in Review.Domain.Assets

---
+++ src/Services/Review/Review.Domain/Assets/Behaviors/File.cs
@@ -0,0 +1,24 @@
+﻿using Articles.Abstractions.Events.Dtos;
+using Review.Domain.Assets.ValueObjects;
+
+namespace Review.Domain.Assets;
+
+public partial class File
+{
+    private File(){}
+
+    internal static File CreateFromSubmission(FileDto fileDto, AssetTypeDefinition assetType)
+    {
+        var extension = FileExtension.FromFileName(fileDto.OriginalName, assetType);
+        var file = new File()
+        {
+            Name = FileName.Create(fileDto.Name),
+            Extension = extension,
+            OriginalName = fileDto.OriginalName,
+            Size =  fileDto.Size,
+            FileServerId = fileDto.FileServerId
+        };
+        
+        return file;
+    }
+}
